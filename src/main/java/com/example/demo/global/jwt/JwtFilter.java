163ac63cf1befff6bbd82f98a3fad1e504972c8b
choiseoji <choiseoji123@naver.com>
2025-04-25 12:07:19 +0900

    feat: jwtFilter 추가중

---
+++ src/main/java/com/example/demo/global/jwt/JwtFilter.java
@@ -0,0 +1,31 @@
+package com.example.demo.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        // 1. 헤더에서 토큰 추출
+
+        // 2. 토큰이 유효하다면 인증 처리
+
+        // 3. 다음 필터로 넘김
+    }
+}
